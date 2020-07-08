744c89058e5f7376b3621cbb138d6fccd8a0a44f
Biggs Thorarensen <biggs@devillion.com>
2020-07-08 12:17:56 +0000

    adds back to dashboard link on accessibility page

--- pages/accessibility.R
+++ pages/accessibility.R
@@ -1,6 +1,11 @@
 accessibility <- function(title, content) {
   tagList(
     tags$title("Accessibility statement - Healthcare associated COVID-19 Surveillance in England"),
+    tags$a(
+        class="govuk-back-link",
+        href="#!/dashboard",
+        "Back to dashboard"
+    ),
     includeMarkdown("content/accessibility.md")
   )
 }

3c727133d40bdd7e8b273e467caa72da1e1bcd92
Biggs Thorarensen <biggs@devillion.com>
2020-07-23 16:22:14 +0000

    adds cookie page content

---
+++ content/cookies.md
@@ -0,0 +1,25 @@
+## Cookies
+
+Cookies are files saved on your phone, tablet or computer when you visit a website.
+
+We use cookies to store information about how you use the data.gov.uk website, such as the pages you visit.
+
+### Cookie settings
+
+We use 2 types of cookie. You can choose which cookies you're happy for us to use.
+
+### Cookies that measure website use
+
+We use Google Analytics to measure how you use the website so we can improve it based on user needs. Google Analytics sets cookies that store anonymised information about:
+
+- how you got to the site
+- the pages you visit on data.gov.uk, and how long you spend on each page
+- what you click on while you're visiting the site
+
+We do not allow Google to use or share the data about how you use this site.
+
+These are the Google Analytics cookies we’ll use:
+
+| Name           | Purpose      | Expires      |
+| :------------- | :----------: | -----------: |
+|  _ga, _gid   | These help us count how many people visit data.gov.uk by tracking if you’ve visited before   | _ga 2 years, _gid 24 hours   |
---
+++ pages/cookies.R
@@ -0,0 +1,70 @@
+cookies <- function(title, content) {
+  tagList(
+    tags$title("Cookies - Healthcare associated COVID-19 Surveillance in England"),
+    tags$a(
+        class="govuk-back-link js-gototop",
+        href="#!/dashboard",
+        "Back to dashboard"
+    ),
+    tags$div(
+        class="util-text-max-width",
+        tags$div(
+            class="markdown",
+            includeMarkdown("content/cookies.md")
+        ),
+        tags$div(
+            class="govuk-form-group",
+            tags$div(
+                class="govuk-radios",
+                tags$div(
+                    class="govuk-radios__item",
+                    tags$input(
+                        class="govuk-radios__input",
+                        id="allow-usage-cookies",
+                        name="usage-cookies",
+                        type="radio",
+                        value="allow"
+                    ),
+                    tags$label(
+                        class="govuk-label govuk-radios__label",
+                        "for"="allow-usage-cookies",
+                        "Use cookies that measure my website use"
+                    )
+                ),
+                tags$div(
+                    class="govuk-radios__item",
+                    tags$input(
+                        class="govuk-radios__input",
+                        id="disallow-usage-cookies",
+                        name="usage-cookies",
+                        type="radio",
+                        value="disallow"
+                    ),
+                    tags$label(
+                        class="govuk-label govuk-radios__label",
+                        "for"="disallow-usage-cookies",
+                        "Do not use cookies that measure my website use"
+                    )
+                )
+            )
+        ),
+        tags$h2(
+            class="govuk-heading-m",
+            "Strictly necessary cookies"
+        ),
+        tags$p(
+            class="govuk-body",
+            "These essential cookies do things like remember your cookie preferences, so we don't ask for them again."
+        ),
+        tags$p(
+            class="govuk-body",
+            "They always need to be on."
+        ),
+        tags$button(
+            class="govuk-button js-cookies-submit",
+            type="submit",
+            "Save changes"
+        )
+    )
+  )
+}
